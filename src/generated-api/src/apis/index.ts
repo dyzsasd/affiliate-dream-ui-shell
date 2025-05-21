7624ee7c4f7d776fd7ddb25a90649c6664e15dce
Shuai <shuai@jinko.cx>
2025-05-21 15:45:08 +0200

    ADD: backend clients

---
+++ src/generated-api/src/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdvertisersApi';
+export * from './AffiliatesApi';
+export * from './CampaignsApi';
+export * from './OrganizationsApi';
+export * from './ProfileApi';
+export * from './SystemApi';
+export * from './WebhooksApi';
---
+++ src/generated-api/src/models/index.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DomainAdvertiser';
+export * from './DomainAdvertiserProviderMapping';
+export * from './DomainAffiliate';
+export * from './DomainAffiliateProviderMapping';
+export * from './DomainCampaign';
+export * from './DomainCampaignProviderOffer';
+export * from './DomainOrganization';
+export * from './DomainProfile';
+export * from './HandlersCreateAdvertiserProviderMappingRequest';
+export * from './HandlersCreateAdvertiserRequest';
+export * from './HandlersCreateAffiliateProviderMappingRequest';
+export * from './HandlersCreateAffiliateRequest';
+export * from './HandlersCreateCampaignProviderOfferRequest';
+export * from './HandlersCreateCampaignRequest';
+export * from './HandlersCreateOrganizationRequest';
+export * from './HandlersProfileRequest';
+export * from './HandlersSupabaseUserWebhookPayload';
+export * from './HandlersSupabaseUserWebhookPayloadRecord';
+export * from './HandlersUpdateAdvertiserProviderMappingRequest';
+export * from './HandlersUpdateAdvertiserRequest';
+export * from './HandlersUpdateAffiliateProviderMappingRequest';
+export * from './HandlersUpdateAffiliateRequest';
+export * from './HandlersUpdateCampaignProviderOfferRequest';
+export * from './HandlersUpdateCampaignRequest';
+export * from './HandlersUpdateOrganizationRequest';
